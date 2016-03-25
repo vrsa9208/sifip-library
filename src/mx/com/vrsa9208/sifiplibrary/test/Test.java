8b8b2773e77e7b32c8717bef9aed6999fa016471
vrsa9208 <vrsa9208@gmail.com>
2016-03-25 01:47:30 -0600

    Se limpio la clase Test

--- src/mx/com/vrsa9208/sifiplibrary/test/Test.java
+++ src/mx/com/vrsa9208/sifiplibrary/test/Test.java
@@ -5,10 +5,6 @@
  */
 package mx.com.vrsa9208.sifiplibrary.test;
 
-import java.util.List;
-import mx.com.vrsa9208.sifiplibrary.dao.UsuarioDao;
-import mx.com.vrsa9208.sifiplibrary.dao.impl.UsuarioDaoJDBC;
-import mx.com.vrsa9208.sifiplibrary.model.Usuario;
 
 /**
  *
@@ -18,14 +14,5 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("Pruebas de la libreria");
-        UsuarioDao dao = UsuarioDaoJDBC.getInstance();
-        Usuario nuevo = new Usuario();
-        nuevo.setNombre("Víctor Orlando");
-        nuevo.setPrimerApellido("Santiago");
-        nuevo.setSegundoApellido("Sánchez");
-        nuevo.setEmail("vrsa9208@gmail.com");
-        nuevo.setPassword("12345");
-        dao.add(nuevo);
-        List<Usuario> lista = dao.get();
     }
 }
