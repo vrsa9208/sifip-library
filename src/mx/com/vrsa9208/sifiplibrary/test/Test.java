81a1864fe36f515961deae9be8598f7b751362e4
unknown <vrsa9208@gmail.com>
2016-05-24 00:03:35 -0500

    feat: Prueba para sifip-library. Agrega un usuario

--- src/mx/com/vrsa9208/sifiplibrary/test/Test.java
+++ src/mx/com/vrsa9208/sifiplibrary/test/Test.java
@@ -5,6 +5,11 @@
  */
 package mx.com.vrsa9208.sifiplibrary.test;
 
+import java.util.GregorianCalendar;
+import mx.com.vrsa9208.sifiplibrary.dao.UsuarioDao;
+import mx.com.vrsa9208.sifiplibrary.dao.impl.UsuarioDaoJDBC;
+import mx.com.vrsa9208.sifiplibrary.model.Usuario;
+
 
 /**
  *
@@ -14,5 +19,18 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("Pruebas de la libreria");
+        
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Víctor Orlando");
+        usuario.setPrimerApellido("Santiago");
+        usuario.setSegundoApellido("Sánchez");
+        usuario.setPassword("12345");
+        usuario.setFechaCreacion(new GregorianCalendar());
+        usuario.setActivo(true);
+        usuario.setId_perfil(2);
+        
+        UsuarioDao dao = UsuarioDaoJDBC.getInstance();
+        dao.add(usuario);
+        
     }
 }
